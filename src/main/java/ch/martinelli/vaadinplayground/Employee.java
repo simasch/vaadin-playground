b16fdb9a8bec8239006509f02d963c188a088b60
Simon Martinelli <simon@72.services>
2019-08-14 09:11:56 +0200

    Changed Grid to TreeGrid

--- src/main/java/ch/martinelli/vaadinplayground/Employee.java
+++ src/main/java/ch/martinelli/vaadinplayground/Employee.java
@@ -1,16 +1,24 @@
 package ch.martinelli.vaadinplayground;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Employee {
 
-    public Employee(String name, LocalDate birthday) {
-        this.name = name;
-        this.birthday = birthday;
-    }
-
     private String name;
     private LocalDate birthday;
+    private Employee boss;
+    private List<Employee> directs = new ArrayList<>();
+
+    public Employee(String name, LocalDate birthday, Employee... directs) {
+        this.name = name;
+        this.birthday = birthday;
+        for (Employee direct : directs) {
+            direct.boss = this;
+            this.directs.add(direct);
+        }
+    }
 
     public String getName() {
         return name;
@@ -26,5 +34,21 @@ public class Employee {
 
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
+    }
+
+    public Employee getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Employee boss) {
+        this.boss = boss;
+    }
+
+    public List<Employee> getDirects() {
+        return directs;
+    }
+
+    public void setDirects(List<Employee> directs) {
+        this.directs = directs;
     }
 }
--- src/main/java/ch/martinelli/vaadinplayground/IndexView.java
+++ src/main/java/ch/martinelli/vaadinplayground/IndexView.java
@@ -5,25 +5,29 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.editor.Editor;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.treegrid.TreeGrid;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.provider.hierarchy.AbstractBackEndHierarchicalDataProvider;
+import com.vaadin.flow.data.provider.hierarchy.HierarchicalQuery;
 import com.vaadin.flow.function.ValueProvider;
 import com.vaadin.flow.router.Route;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 @Route(value = "index")
 public class IndexView extends VerticalLayout {
 
     public IndexView() {
-        Grid<Employee> grid = new Grid<>();
+        TreeGrid<Employee> treeGrid = new TreeGrid<>();
 
-        Grid.Column<Employee> nameColumn = grid.addColumn((ValueProvider<Employee, String>) employee -> employee.getName())
+        Grid.Column<Employee> nameColumn = treeGrid.addHierarchyColumn((ValueProvider<Employee, String>) employee -> employee.getName())
                 .setHeader("Name");
-        Grid.Column<Employee> birthdayColumn = grid.addColumn((ValueProvider<Employee, LocalDate>) employee -> employee.getBirthday())
+        Grid.Column<Employee> birthdayColumn = treeGrid.addColumn((ValueProvider<Employee, LocalDate>) employee -> employee.getBirthday())
                 .setHeader("Birthday");
 
         Binder<Employee> binder = new Binder<>(Employee.class);
-        Editor<Employee> editor = grid.getEditor();
+        Editor<Employee> editor = treeGrid.getEditor();
         editor.setBinder(binder);
         editor.setBuffered(true);
 
@@ -35,18 +39,44 @@ public class IndexView extends VerticalLayout {
         binder.bind(datePicker, "birthday");
         birthdayColumn.setEditorComponent(datePicker);
 
-        grid.addItemClickListener(event -> {
+        treeGrid.addItemClickListener(event -> {
             editor.save();
             editor.editItem(event.getItem());
         });
 
-        grid.getElement().addEventListener("keyup", event -> editor.cancel())
+        treeGrid.getElement().addEventListener("keyup", event -> editor.cancel())
                 .setFilter("event.key === 'Escape' || event.key === 'Esc'");
 
-        grid.setItems(new Employee("Simon", LocalDate.of(1980, 1, 1)),
-                new Employee("Peter", LocalDate.of(2000, 12, 11)));
 
-        add(grid);
+        Employee direct = new Employee("Peter", LocalDate.of(2000, 12, 11));
+        Employee root = new Employee("Simon", LocalDate.of(1980, 1, 1), direct);
+
+        treeGrid.setDataProvider(new AbstractBackEndHierarchicalDataProvider<Employee, Void>() {
+            @Override
+            public int getChildCount(HierarchicalQuery<Employee, Void> hierarchicalQuery) {
+                if (hierarchicalQuery.getParent() == null) {
+                    return root.getDirects().size();
+                } else {
+                    return hierarchicalQuery.getParent().getDirects().size();
+                }
+            }
+
+            @Override
+            public boolean hasChildren(Employee employee) {
+                return !employee.getDirects().isEmpty();
+            }
+
+            @Override
+            protected Stream<Employee> fetchChildrenFromBackEnd(HierarchicalQuery<Employee, Void> hierarchicalQuery) {
+                if (hierarchicalQuery.getParent() == null) {
+                    return Stream.of(root);
+                } else {
+                    return hierarchicalQuery.getParent().getDirects().stream().skip(hierarchicalQuery.getOffset()).limit(hierarchicalQuery.getLimit());
+                }
+            }
+        });
+
+        add(treeGrid);
     }
 
 }
